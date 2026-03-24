6e2ffaf821229e5647d286229d842eec57c5b98d
salauddin <salauddin@hasan.com>
2026-03-24 07:53:40 +0600

    feat: add reusable MemberProfile component with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,78 @@
- 
-import './App.css'
+import "./App.css";
+import MemberProfile from "./MemberProfile";
+
+const members = [
+  {
+    id: 1,
+    name: "Alice Johnson",
+    role: "Frontend Developer",
+    image: "https://randomuser.me/api/portraits/women/9.jpg",
+  },
+  {
+    id: 2,
+    name: "Bob Smith",
+    role: "Backend Developer",
+    image: "https://randomuser.me/api/portraits/men/20.jpg",
+  },
+  {
+    id: 3,
+    name: "Charlie Brown",
+    role: "UI/UX Designer",
+    image: "https://randomuser.me/api/portraits/women/90.jpg",
+  },
+  {
+    id: 4,
+    name: "Diana Prince",
+    role: "Project Manager",
+    image: "https://randomuser.me/api/portraits/women/71.jpg",
+  },
+  {
+    id: 5,
+    name: "Ethan Hunt",
+    role: "DevOps Engineer",
+    image: "https://randomuser.me/api/portraits/men/77.jpg",
+  },
+  {
+    id: 6,
+    name: "Mark Gallagher",
+    role: "QA Engineer",
+    image: "https://randomuser.me/api/portraits/men/60.jpg",
+  },
+  {
+    id: 7,
+    name: "George Lucas",
+    role: "Data Scientist",
+    image: "https://randomuser.me/api/portraits/men/30.jpg",
+  },
+  {
+    id: 8,
+    name: "Hannah Baker",
+    role: "Content Strategist",
+    image: "https://randomuser.me/api/portraits/women/79.jpg",
+  },
+];
 
 function App() {
- 
-
   return (
     <>
-      <h1 className='text-center'>hello dev</h1>
+      <div className="min-h-screen p-6">
+        <h1 className="text-center text-3xl font-bold text-blue-800 pb-6">
+          Team Members List App
+        </h1>
+
+        <div className="grid md:grid-cols-3 gap-4 ">
+          {members.map((member) => (
+          <MemberProfile
+            key={member.id}
+            name={member.name}
+            role={member.role}
+            img={member.image}
+          />
+        ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/MemberProfile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MemberProfile = ({ name, role, img }) => {
+  return (
+    <div className="bg-white shadow-md rounded-lg border border-gray-200 p-4 ">
+      <img
+        src={img}
+        alt=""
+        className="w-32 h-32 mx-auto mb-4 object-cover rounded-full"
+      />
+      <h2 className="text-center text-xl text-gray-600 font-semibold">
+        {name}
+      </h2>
+
+      <p className="text-center text-gray-500">{role}</p>
+    </div>
+  );
+};
+
+export default MemberProfile;
